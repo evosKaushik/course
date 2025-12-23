f909a589ea7ad098ff8c9d680e1250c315e33bba
Kaushik <kaushik.lf07@gmail.com>
2025-12-23 10:14:10 +0530

    Procodrr/Redux Selectors in Redux

---
+++ Procodrr/Complete Frontend/Redux/17.Selectors-In-Redux/store/index.js
@@ -0,0 +1,13 @@
+import productsReducer from "./slices/productsSlice";
+import cartReducer from "./slices/cartSlice";
+import wishListReducer from "./slices/wishListSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "./slices/middleware/logger";
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cartItems: cartReducer,
+    wishList: wishListReducer,
+  },
+});
