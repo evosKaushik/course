8e066359f2236159415f8b94e7c69c90e0c349b0
Kaushik <kaushik.lf07@gmail.com>
2025-12-27 21:33:55 +0530

    Procodrr/Backend What is the Different Between CLI and GUI

---
+++ Procodrr/Next.Js/Section-3_Rendering-Paradigms-in-Next.js/05_What-Is-ISR/app/blogs/[blogID]/page.js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+export const dynamicParams = false;
+export const revalidate = 5;
+
+
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  return data.map(({ id }) => ({ blogID: id.toString() }));
+  // return [
+  //   { blogID: "1" },
+  //   { blogID: "2" },
+  //   { blogID: "3" },
+  //   { blogID: "4" },
+  //   { blogID: "5" },
+  // ];
+}
+
+const Blogs = async ({ params }) => {
+  const { blogID } = await params;
+  console.log(blogID);
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our Blog {blogID}</h1>
+        <h2>Date: {new Date().toLocaleString()}</h2>
+        <p>This is blog {blogID} page.</p>
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
