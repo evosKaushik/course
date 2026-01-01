af8a466aed4b6fa65b1dba09f103977d1372a15a
Kaushik <kaushik.lf07@gmail.com>
2026-01-01 19:48:02 +0530

    Procodrr/Next.Js Hydration

---
+++ Procodrr/Next.Js/Section-03_Rendering-Paradigms-in-Next.js/09_Hydration-In-Next.Js/app/blogs/page.js
@@ -0,0 +1,68 @@
+"use client";
+import Comments from "@/components/Comments";
+import Likes from "@/components/Likes";
+import Views from "@/components/Views";
+import Link from "next/link";
+import { Suspense } from "react";
+
+const Blogs = () => {
+  console.log("Blogs");
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our Blog</h1>
+        {/* <button onClick={() => console.log("Button Clicked!")}>
+          Click Me!
+        </button> */}
+        <ol className="blog-links">
+          <li>
+            <Link href="/blogs/1">Blog 1</Link>
+          </li>
+          <li>
+            <Link href="/blogs/2">Blog 2</Link>
+          </li>
+          <li>
+            <Link href="/blogs/3">Blog 3</Link>
+          </li>
+        </ol>
+        <br />
+        <div className="blog-links">
+          <Suspense fallback={<div>Loading Likes....</div>}>
+            <Likes />
+          </Suspense>
+          <Comments />
+          <Views />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
+
+// Servers components execute only on the server
+// Client components execute on the server as well as on the client
