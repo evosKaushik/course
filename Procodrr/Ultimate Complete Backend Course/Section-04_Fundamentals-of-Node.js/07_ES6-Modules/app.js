89323998e12c0b80e6dda1a559770f672803231b
Kaushik <kaushik.lf07@gmail.com>
2026-01-19 09:01:33 +0530

    Procodrr/Backend ES6 Modules

---
+++ Procodrr/Ultimate Complete Backend Course/Section-04_Fundamentals-of-Node.js/07_ES6-Modules/app.js
@@ -0,0 +1,8 @@
+console.log('App.js')
+
+
+import num, { a as c} from "./math.js";
+
+var a = 5
+console.log(c);
+console.log(num);
---
+++ Procodrr/Ultimate Complete Backend Course/Section-04_Fundamentals-of-Node.js/07_ES6-Modules/math.js
@@ -0,0 +1,4 @@
+console.log('Math.js')
+const num = 5;
+export const a = 10;
+export default num;
