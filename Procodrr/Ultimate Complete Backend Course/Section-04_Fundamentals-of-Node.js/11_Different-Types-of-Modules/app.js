fb8a520e5dbc58beb23f023092d842dbe6c84aa4
Kaushik <kaushik.lf07@gmail.com>
2026-01-20 09:56:28 +0530

    Procodrr/Backend Different Types of Modules

---
+++ Procodrr/Ultimate Complete Backend Course/Section-04_Fundamentals-of-Node.js/11_Different-Types-of-Modules/app.js
@@ -0,0 +1,10 @@
+// import fs from "fs"; //Native module or Core module or internal module
+import fs from "node:fs"; //Same but Best practice to know that this is node js module
+import net from "node:net";
+import dgram from "node:dgram";
+import http from "node:http";
+import https from "node:https";
+
+import { num } from "./math.js"; //User module
+
+import axios from "axios"; //Third party module or external module or NPM module
