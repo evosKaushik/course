445bee6636edcf8f658cc839e3171321a4815efc
Kaushik <kaushik.lf07@gmail.com>
2026-02-19 17:12:12 +0530

    Procodrr/Backend BASE64 In NODEJS

--- Procodrr/Ultimate Backend with Node js/Section-06_Mastering-Buffers-Handling-Binary-Data-in-Node.js/15_Base-64-in-Node.js/app.js
+++ Procodrr/Ultimate Backend with Node js/Section-06_Mastering-Buffers-Handling-Binary-Data-in-Node.js/15_Base-64-in-Node.js/app.js
@@ -1,14 +1,17 @@
 import fs from "node:fs/promises";
 
-
 //btoa()
 // const a = await fs.readFile("file.txt", "base64")
-const bufferContent = await fs.readFile("file.txt")
+// const bufferContent = await fs.readFile("favicon/favicon-16x16.png")
 
-const a = bufferContent.toString('base64')
+// const a = bufferContent.toString('base64')
 
-console.log(a)
+// console.log(a)
 
 //atob()
-fs.writeFile("new-file.txt",a);
+// fs.writeFile("new-file.txt",a);
 // fs.writeFile("new-file.txt", "YWJj", "base64");
+
+const imageBase64String = await fs.readFile("new-file.txt", "utf-8");
+
+fs.writeFile("smile.png", imageBase64String, "base64");
