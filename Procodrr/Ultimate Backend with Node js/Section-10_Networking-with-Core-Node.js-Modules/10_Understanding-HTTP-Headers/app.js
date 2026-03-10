a2ccd01f6d0312d04da677da3b5a583000b6c21e
Kaushik <kaushik.lf07@gmail.com>
2026-03-10 17:00:44 +0530

    Procodrr/Backend Understanding HTTP Headers

---
+++ Procodrr/Ultimate Backend with Node js/Section-10_Networking-with-Core-Node.js-Modules/10_Understanding-HTTP-Headers/app.js
@@ -0,0 +1,41 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import net from "node:net";
+
+const server = net.createServer((socket) => {
+//   socket.write(
+//     `HTTP/1.1 200 Okay
+// Access-Control-Allow-Origin:*
+// Access-Control-Expose-Headers:*\n\n`,
+//   );
+socket.write('HTTP/1.1 200 Okay\n')
+socket.write('Access-Control-Allow-Origin: *\n')
+socket.write('Access-Control-Expose-Headers: *\n')
+socket.write('\n\n')
+
+  // socket.end()
+  const readStream = createReadStream("video.mp4");
+  // const readStream = createReadStream("river.webp");
+  // const readStream = createReadStream("numbers.txt");
+  readStream.pipe(socket);
+ 
+  readStream.on("end", () => {
+    console.log("File ended");
+  });
+
+  socket.on("data", (chunk) => {
+    console.log(chunk.toString());
+  });
+
+  socket.on("close", () => {
+    console.log(socket.remoteAddress, ": Client disconnected");
+  });
+
+  socket.on("error", () => {
+    console.log("Client Lost");
+  });
+  console.log("Client Connected", socket.remoteAddress);
+});
+
+server.listen(4000, "0.0.0.0", () => {
+  console.log("Server started on port 4000");
+});
