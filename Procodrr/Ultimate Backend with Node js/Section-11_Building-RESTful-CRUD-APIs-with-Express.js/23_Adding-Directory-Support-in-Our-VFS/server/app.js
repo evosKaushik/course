ab30712914de6df12e2a1adec2b0018c5c20c0a6
Kaushik <kaushik.lf07@gmail.com>
2026-03-21 11:38:47 +0530

    Procodrr/Backend Adding Directory Support in Our VFS

---
+++ Procodrr/Ultimate Backend with Node js/Section-11_Building-RESTful-CRUD-APIs-with-Express.js/23_Adding-Directory-Support-in-Our-VFS/server/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import directoryRoutes from "./routes/directory.routes.js";
+import filesRoutes from "./routes/file.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/directory", directoryRoutes);
+app.use("/file", filesRoutes);
+
+app.listen(4000, () => {
+  console.log(`Server Started`);
+});
