ee779c39607954a0dfb6f307bf5bfffaf4c9ece1
Kaushik <kaushik.lf07@gmail.com>
2026-03-23 10:24:19 +0530

    Procodrr/Backend Error Handling in Express

---
+++ Procodrr/Ultimate Backend with Node js/Section-11_Building-RESTful-CRUD-APIs-with-Express.js/28_Error-Handling-in-Express/server/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import directoryRoutes from "./routes/directoryRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/directory", directoryRoutes);
+app.use("/file", fileRoutes);
+
+app.use((err, req, res, next) => {
+  console.log("Error Occurred");
+  res.status(err.status || 500).json({
+    message: "Something went wrong!",
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`Server Started`);
+});
