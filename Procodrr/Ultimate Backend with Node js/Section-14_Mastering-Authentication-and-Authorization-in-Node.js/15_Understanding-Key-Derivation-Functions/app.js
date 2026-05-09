77db8eb1f62ab2653bf4f80cb9939cc6bb687c27
Kaushik <kaushik.lf07@gmail.com>
2026-05-09 22:29:15 +0530

    Procodrr/Backend Understanding Key Derivation Functions

---
+++ Procodrr/Ultimate Backend with Node js/Section-14_Mastering-Authentication-and-Authorization-in-Node.js/15_Understanding-Key-Derivation-Functions/app.js
@@ -0,0 +1,9 @@
+import crypto from "node:crypto";
+
+const salt = crypto.randomBytes(16)
+
+console.log(salt.byteLength)
+
+crypto.pbkdf2("password", salt, 100000, 32, "sha256", (err, output) => {
+  console.log(output.toString("base64url"));
+});
