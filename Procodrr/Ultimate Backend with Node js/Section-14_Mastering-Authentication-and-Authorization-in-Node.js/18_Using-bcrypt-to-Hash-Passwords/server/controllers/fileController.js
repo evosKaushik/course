06d8812dd641629f03ec63a2547ae6db698d4616
Kaushik <kaushik.lf07@gmail.com>
2026-05-10 23:39:18 +0530

    Procodrr/Backend Using bcrypt to Hash Passwords

---
+++ Procodrr/Ultimate Backend with Node js/Section-14_Mastering-Authentication-and-Authorization-in-Node.js/18_Using-bcrypt-to-Hash-Passwords/server/controllers/fileController.js
@@ -0,0 +1,113 @@
+import { createWriteStream } from "fs";
+import { rm } from "fs/promises";
+import path from "path";
+import Directory from "../models/directoryModel.js";
+import File from "../models/fileModel.js";
+
+export const uploadFile = async (req, res, next) => {
+  const parentDirId = req.params.parentDirId || req.user.rootDirId;
+
+  const parentDirData = await Directory.findOne({
+    _id: parentDirId,
+    userId: req.user._id,
+  }).lean();
+
+  // Check if parent directory exists
+  if (!parentDirData) {
+    return res.status(404).json({ error: "Parent directory not found!" });
+  }
+
+  const filename = req.headers.filename || "untitled";
+  const extension = path.extname(filename);
+
+  const insertedFile = await File.insertOne({
+    extension,
+    name: filename,
+    parentDirId: parentDirData._id,
+    userId: req.user._id,
+  });
+
+  const fileId = insertedFile._id.toString();
+
+  const fullFileName = `${fileId}${extension}`;
+
+  const writeStream = createWriteStream(`./storage/${fullFileName}`);
+  req.pipe(writeStream);
+
+  req.on("end", async () => {
+    return res.status(201).json({ message: "File Uploaded" });
+  });
+
+  req.on("error", async () => {
+    await File.deleteOne({ _id: insertedFile._id });
+    return res.status(404).json({ message: "Could not Upload File" });
+  });
+};
+
+export const getFile = async (req, res) => {
+  const { id } = req.params;
+  const fileData = await File.findOne({
+    _id: id,
+    userId: req.user._id,
+  }).lean();
+  // Check if file exists
+  if (!fileData) {
+    return res.status(404).json({ error: "File not found!" });
+  }
+
+  // If "download" is requested, set the appropriate headers
+  const filePath = `${process.cwd()}/storage/${id}${fileData.extension}`;
+
+  if (req.query.action === "download") {
+    return res.download(filePath, fileData.name);
+  }
+
+  // Send file
+  return res.sendFile(filePath, (err) => {
+    if (!res.headersSent && err) {
+      return res.status(404).json({ error: "File not found!" });
+    }
+  });
+};
+
+export const renameFile = async (req, res, next) => {
+  const { id } = req.params;
+  let a  = 12;
+  const fileData = await File.findOne({
+    _id: id,
+    userId: req.user._id,
+  }).lean();
+
+  // Check if file exists
+  if (!fileData) {
+    return res.status(404).json({ error: "File not found!" });
+  }
+
+  try {
+    await File.findOneAndUpdate({ _id: id }, { name: req.body.newFilename });
+    return res.status(200).json({ message: "Renamed" });
+  } catch (err) {
+    err.status = 500;
+    next(err);
+  }
+};
+
+export const deleteFile = async (req, res, next) => {
+  const { id } = req.params;
+  const fileData = await File.findOne({
+    _id: id,
+    userId: req.user._id,
+  }).lean();
+
+  if (!fileData) {
+    return res.status(404).json({ error: "File not found!" });
+  }
+
+  try {
+    await rm(`./storage/${id}${fileData.extension}`);
+    await File.findByIdAndDelete(fileData._id);
+    return res.status(200).json({ message: "File Deleted Successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
