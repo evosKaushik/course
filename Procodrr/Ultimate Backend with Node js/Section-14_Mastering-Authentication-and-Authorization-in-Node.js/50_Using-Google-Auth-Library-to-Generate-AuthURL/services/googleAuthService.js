8b8957dbee32e273a4ed88937743842b2b097738
Kaushik <kaushik.lf07@gmail.com>
2026-05-23 23:09:19 +0530

    Procodrr/Backend Using Google Auth Library to Generate AuthURL

---
+++ Procodrr/Ultimate Backend with Node js/Section-14_Mastering-Authentication-and-Authorization-in-Node.js/50_Using-Google-Auth-Library-to-Generate-AuthURL/services/googleAuthService.js
@@ -0,0 +1,28 @@
+import { OAuth2Client } from "google-auth-library";
+
+const clientId =
+  "761529584102-cu9i5gvl4vdfjkqdjfjpcb19rhfumqh1.apps.googleusercontent.com";
+
+const redirectUrl = "http://localhost:4000/auth/google/callback";
+const client = new OAuth2Client({
+  clientId,
+  clientSecret,
+  redirectUri: redirectUrl,
+});
+
+export const authUrl = client.generateAuthUrl({
+  scopes: ["profile", "email", "openid"],
+});
+
+export async function fetchUserFromGoogle(code) {
+  const { tokens } = await client.getToken(code);
+
+  const loginTicket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: clientId,
+  });
+
+  const userData = loginTicket.getPayload();
+  console.log(userData);
+  return userData;
+}
