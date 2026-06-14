4511e1d173372ade7e64e5ebb145b26642a592fa
Kaushik <kaushik.lf07@gmail.com>
2026-06-15 02:41:31 +0530

    Procodrr/Typescript What Are Types?

---
+++ Procodrr/The Ultimate TypeScript Course/Section-02_TypeScript-Fundamentals-and-Compiler-Configuration/01_What-Are-Types?/app.ts
@@ -0,0 +1,22 @@
+let num: number = 5
+
+
+console.log(typeof num)
+
+//! Data Types 
+
+// Primitive 
+/* 
+Number
+String
+Boolean
+Bigint
+Undefined
+Null
+Symbol
+*/
+
+// Non-Primitive
+/*
+Object
+*/
