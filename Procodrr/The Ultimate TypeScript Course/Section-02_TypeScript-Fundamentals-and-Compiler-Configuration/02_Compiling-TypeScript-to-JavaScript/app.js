1ea68c64bb9e2d9501103027f3f89cfe95d03846
Kaushik <kaushik.lf07@gmail.com>
2026-06-15 11:18:46 +0530

    Procodrr/Typescript Compiling TypeScript to JavaScript

---
+++ Procodrr/The Ultimate TypeScript Course/Section-02_TypeScript-Fundamentals-and-Compiler-Configuration/02_Compiling-TypeScript-to-JavaScript/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let num = 5;
+console.log(typeof num);
+// num = "string"
+// console.log(typeof num)
+let a = 123;
---
+++ Procodrr/The Ultimate TypeScript Course/Section-02_TypeScript-Fundamentals-and-Compiler-Configuration/02_Compiling-TypeScript-to-JavaScript/app.ts
@@ -0,0 +1,9 @@
+let num = 5
+
+console.log(typeof num)
+
+// num = "string"
+
+// console.log(typeof num)
+
+let a = 123
