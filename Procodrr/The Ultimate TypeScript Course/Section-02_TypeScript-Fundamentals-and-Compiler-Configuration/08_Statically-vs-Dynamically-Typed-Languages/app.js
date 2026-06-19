90982797b42e9b2fbfbcc25b8028ca5254a45fca
Kaushik <kaushik.lf07@gmail.com>
2026-06-19 22:29:33 +0530

    Procodrr/Typescript Statically vs Dynamically Typed Languages

---
+++ Procodrr/The Ultimate TypeScript Course/Section-02_TypeScript-Fundamentals-and-Compiler-Configuration/08_Statically-vs-Dynamically-Typed-Languages/app.js
@@ -0,0 +1,11 @@
+// let a = 5
+
+
+// a.toUpperCase()
+
+const user = {
+
+    name: "Kaushik"
+}
+
+console.log(user.age)
---
+++ Procodrr/The Ultimate TypeScript Course/Section-02_TypeScript-Fundamentals-and-Compiler-Configuration/08_Statically-vs-Dynamically-Typed-Languages/app.ts
@@ -0,0 +1,10 @@
+// let b = 5
+
+// b.toUpperCase()
+
+const user = {
+    name: "Kaushik",
+    age: 40
+}
+
+console.log(user.age())
