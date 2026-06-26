952f79bba6bbaf501934ccd74c2fd4ff93c8b1e0
Kaushik <kaushik.lf07@gmail.com>
2026-06-26 23:54:04 +0530

    Procodrr/Backend/Typescript Readonly Arrays and Tuples

---
+++ Procodrr/The Ultimate TypeScript Course/Section-03_Arrays-and-Tuples-in-TypeScript/04_Readonly-Arrays-and-Tuples/app.ts
@@ -0,0 +1,9 @@
+// const a: readonly [name: string, age: number] = ["Kaushik", 7]
+// // const b = ["ProcodrrT-Shirt", 10]
+
+// // a.pop()
+
+// a[1].toString()
+
+const a: readonly number[] = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
+
