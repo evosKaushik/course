b72c8e2b21e43bbd373081856f6de075c6a30ea1
Kaushik <kaushik.lf07@gmail.com>
2026-06-27 00:22:48 +0530

    Procodrr/Typescript  What are Type Aliases?

---
+++ Procodrr/The Ultimate TypeScript Course/Section-04_Type-Aliases,-Literal-Types,-and-Type-Assertions/01_What-are-Type-Aliases?/app.js
@@ -0,0 +1,5 @@
+const a = ["Kaushik", 7];
+// const b = ["ProcodrrT-Shirt", 10]
+a.pop();
+a[0].toUpperCase();
+export {};
---
+++ Procodrr/The Ultimate TypeScript Course/Section-04_Type-Aliases,-Literal-Types,-and-Type-Assertions/01_What-are-Type-Aliases?/app.ts
@@ -0,0 +1,7 @@
+type str = string
+type num = number
+
+const alphabet: str = "hi"
+const number: num = 7
+
+console.log(alphabet)
