9f0112f6260f4599d6cf64650b8a64418c1d4160
Kaushik <kaushik.lf07@gmail.com>
2026-06-27 23:39:59 +0530

    Procodrr/Typescript Template Literal Types

---
+++ Procodrr/The Ultimate TypeScript Course/Section-04_Type-Aliases,-Literal-Types,-and-Type-Assertions/03_Template-Literal-Types/app.ts
@@ -0,0 +1,7 @@
+type AvailableFruit = "Apple" | "Papaya" | "Mango" | "Grapes"
+type DicNumber = 1 | 2 | 3 | 4 | 5 | 6
+
+type EndPoints = `/api/${("user" | "posts" | "comments")}`
+
+let api: EndPoints = "/api/posts"
+
