93a050ee8a210de6d1be005a502a4977dfa013eb
Kaushik <kaushik.lf07@gmail.com>
2026-07-09 00:37:10 +0530

    Procodrr/Typescript Optional Parameters in Functions and Methods

---
+++ Procodrr/The Ultimate TypeScript Course/Section-05_Objects-&-Functions-Typing-in-Depth/11_Optional-Parameters-in-Functions-and-Methods/app.js
@@ -0,0 +1,5 @@
+function greet(name = "") {
+    console.log(`Hi,${name} How are you?`);
+}
+greet();
+export {};
---
+++ Procodrr/The Ultimate TypeScript Course/Section-05_Objects-&-Functions-Typing-in-Depth/11_Optional-Parameters-in-Functions-and-Methods/app.ts
@@ -0,0 +1,5 @@
+function greet(name1?: string, name2?:string) {
+  console.log(`Hi,${name} How are you?`);
+}
+
+greet();
