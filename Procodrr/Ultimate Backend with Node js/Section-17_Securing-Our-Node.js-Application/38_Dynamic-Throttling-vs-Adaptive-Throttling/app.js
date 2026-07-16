58111ac354ffa08453e32856afbe2002784a8c5e
Kaushik <kaushik.lf07@gmail.com>
2026-07-17 00:52:27 +0530

    Procodrr/Backend Dynamic Throttling vs Adaptive Throttling

---
+++ Procodrr/Ultimate Backend with Node js/Section-17_Securing-Our-Node.js-Application/38_Dynamic-Throttling-vs-Adaptive-Throttling/app.js
@@ -0,0 +1,62 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { rateLimit } from "express-rate-limit";
+
+const app = express();
+const PORT = 4000;
+
+const globalLimiter = rateLimit({
+  windowMs: 20_0000,
+  limit: 5,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
+app.use(globalLimiter);
+app.use(express.static("./public"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const throttleObj = {};
+
+let previousDelay = 0;
+let lastRequestTime;
+
+function throttle(waitTime = 2000, allowRequest = 2) {
+  return (req, res, next) => {
+    const now = Date.now();
+
+    const ip = req.ip;
+
+    const { previousDelay, lastRequestTime } = throttleObj[ip] || {
+      lastRequestTime: now,
+      previousDelay: now - waitTime,
+    };
+
+    const timePassed = now - lastRequestTime;
+    const delay = Math.max(0, waitTime + previousDelay - timePassed);
+    throttleObj[ip] = {
+      lastRequestTime: now,
+      previousDelay: delay,
+    };
+    setTimeout(next, delay);
+  };
+}
+
+app.use(throttle);
+
+app.get("/api", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/register", async (req, res) => {
+  bcrypt.hashSync("123456", 14);
+  return res.json({ message: "Registered Successfully" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Visit http://localhost:${PORT}`);
+});
