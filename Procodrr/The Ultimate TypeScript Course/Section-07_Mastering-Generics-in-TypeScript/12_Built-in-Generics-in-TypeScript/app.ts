261d57f9a15abab037d0ac67c7d686afbeb14160
kaushik <kaushik.lf07@gmail.com>
2026-08-29 23:41:30 +0530

    Procodrr/Backend Built-in Generics in TypeScript

---
+++ Procodrr/The Ultimate TypeScript Course/Section-07_Mastering-Generics-in-TypeScript/12_Built-in-Generics-in-TypeScript/app.ts
@@ -0,0 +1,15 @@
+const arr = new Array<boolean>();
+
+arr.push(false);
+
+let a: Array<boolean> = [!false, false];
+
+const myMap = new Map();
+
+myMap.set(false, 10);
+
+const mySet = new Set();
+
+mySet.add("hi");
+mySet.add(2);
+mySet.add(false);
