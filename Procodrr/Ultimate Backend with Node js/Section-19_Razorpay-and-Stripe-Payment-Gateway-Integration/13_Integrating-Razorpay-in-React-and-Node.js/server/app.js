6b7a3226ee56dde2716f64fc2232ddac68c49c64
kaushik <kaushik.lf07@gmail.com>
2026-09-02 00:40:16 +0530

    Procodrr/Backend Integrating Razorpay in React and Node.js

---
+++ Procodrr/Ultimate Backend with Node js/Section-19_Razorpay-and-Stripe-Payment-Gateway-Integration/13_Integrating-Razorpay-in-React-and-Node.js/server/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import data from "./courses.json" with { type: "json" };
+import cors from "cors";
+import Razorpay from "razorpay";
+
+const rzpInstance = new Razorpay({
+  key_id: "rzp_test_TSuRR3SQK1URH1",
+  key_secret: "gJMJ2ci8WqLOCbs4UW6G9qFM",
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/complete-order", async (req, res) => {
+  const { orderId } = req.body;
+  const order = await rzpInstance.orders.fetch(order);
+
+  if (!order) {
+    return res.status(404).json({ message: "Invalid order id" });
+  }
+
+  return res.json({ message: "Order Created" });
+});
+
+app.post("/create-order", async (req, res) => {
+  const { id: courseId } = req.body;
+
+  const course = data.find((course) => course.id === courseId);
+  const order = await rzpInstance.orders.create({
+    amount: course.price * 100,
+    currency: "INR",
+  });
+  res.json({ orderId: order.id });
+});
+
+app.listen(4000, () => {
+  console.log("Server started");
+});
